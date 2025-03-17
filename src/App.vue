8bd4efb09efe0bafaa77eed8b1052934e47b17b0
william-wv <wwillveiga@hotmail.com>
2025-03-17 19:30:24 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+
+
+</script>
+
+<template>
+<header>
+  <HeaderComponent/>
+
+  <main>
+    <RouterView/>
+  </main>
+
+  <footer>
+    <FooterComponent/>
+  </footer>
+</header>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <p>bbbb</p>
+</template>
+
+<style scoped>
+</style>
